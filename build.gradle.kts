e91a16c764a5e38e2da2e7a559d734e93e082857
tezeku <cuzyuhin@yandex.ru>
2026-04-05 19:36:11 +0300

    config: database connection and server configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,13 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+
+
 }
 
 kotlin {
